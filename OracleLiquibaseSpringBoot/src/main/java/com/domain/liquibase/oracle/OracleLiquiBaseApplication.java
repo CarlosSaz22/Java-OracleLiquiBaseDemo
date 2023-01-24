2e4f6472f2769494ffd6ba0b1e3f0e4186f06cd1
CarlosSaz22 <61812538+CarlosSaz22@users.noreply.github.com>
2023-01-24 17:22:43 -0600

    Add project demo liquibase and oracle

---
+++ OracleLiquibaseSpringBoot/src/main/java/com/domain/liquibase/oracle/OracleLiquiBaseApplication.java
@@ -0,0 +1,13 @@
+package com.domain.liquibase.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleLiquiBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleLiquiBaseApplication.class, args);
+	}
+
+}
